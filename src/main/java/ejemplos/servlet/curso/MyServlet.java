72573d00f07bd23ecc7fb0f3134651ae970e2b2b
GendersonAAA <80746925+GendersonAAA@users.noreply.github.com>
2024-10-26 20:03:10 +0200

    modificamos el ejemplo para agregar un formulario

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+						 HttpServletResponse response) throws ServletException, IOException {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
@@ -24,7 +24,31 @@ public class MyServlet extends HttpServlet {
 		out.println("<h1>Ejemplo Servlet</h1>");
 		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
 		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+
+		// Formulario con campo nombre
+		out.println("<form method='POST' action='/myServlet'>");
+		out.println(" <label for='nombre'>Nombre:</label>");
+		out.println(" <input type='text' id='nombre' name='nombre'>");
+		out.println(" <input type='submit' value='Enviar'>");
+		out.println("</form>");
+
 		out.println("</body></html>");
 	}
 
+	protected void doPost(HttpServletRequest request,
+						  HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String nombre = request.getParameter("nombre");
+
+		// send HTML page to client
+		out.println("<html>");
+		out.println("<head><title>Respuesta del Formulario</title></head>");
+		out.println("<body>");
+		out.println("<h1>Hola " + nombre + "!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
 }
